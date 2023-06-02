4fc89efd9427a65b56f0975663b810c861b0a799
Dmitry <markelovdima408@gmail.com>
2023-06-02 21:42:13 +0400

    delete some shit

--- src/components/src/menu/Menu.js
+++
@@ -1,7 +0,0 @@
-class Menu extends Component {
-    addEventListener() {
-        document.querySelectorAll('.menu-item')
-            .forEach(button => button.addEventListener('click',
-                (event) => this.callbacks.showMenuItem(event.target.dataset.item)));
-    }
-}
--- src/components/src/menu/MenuTemplate.js
+++
@@ -1,6 +0,0 @@
-Template.prototype.Menu = () =>
-    `<div>
-        <button data-item='Calculator' class="menu-item">Калькулятор</button>
-        <button data-item='Polynomial' class="menu-item">Полиномы</button>
-        <button data-item='Graph3D' class="menu-item">Фигуры</button>
-    </div>`
